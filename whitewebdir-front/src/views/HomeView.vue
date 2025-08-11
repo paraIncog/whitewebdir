c557c6f25f0f55fb4bd31366fa8da540255a83bc
Andreas Selge <selgeandreas02@gmail.com>
2025-08-11 12:28:20 +0300

    cleanup

--- whitewebdir-front/src/views/HomeView.vue
+++ whitewebdir-front/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to the Home Page</h1>
   </main>
 </template>
